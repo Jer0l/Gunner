b2c303c403ffdb5fc06b79a86a45c8a62fb49200
Jer0l <165727458+Jer0l@users.noreply.github.com>
2026-09-15 16:17:37 -0300

    Actualizar RoomDoor.cs

--- Assets/Scripts/Rooms/RoomDoor.cs
+++ Assets/Scripts/Rooms/RoomDoor.cs
@@ -1,33 +1,36 @@
 using UnityEngine;
 
 public class RoomDoor : MonoBehaviour
-{
-    [SerializeField] private Transform nextRoomPoint;
+ {
+ [SerializeField] private Transform nextRoomPoint;
 
-    private RoomController roomController;
-    private bool unlocked;
+ private RoomController roomController;
+ private bool unlocked;
 
-    public Transform NextRoomPoint => nextRoomPoint;
+ public Transform NextRoomPoint => nextRoomPoint;
 
-    public void Initialize(RoomController controller)
-    {
-        roomController = controller;
-        unlocked = false;
-    }
+ public void Initialize(RoomController controller)
+ {
+ roomController = controller;
+ unlocked = false;
+ }
 
-    public void Unlock()
-    {
-        unlocked = true;
-    }
+ public void Unlock()
+ {
+ unlocked = true;
+ Debug.Log("Puertas Desbloqueadas");
+ }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (!unlocked)
-            return;
+ private void OnTriggerEnter2D(Collider2D other)
+ {
+ if (!unlocked)
+ {
+ Debug.Log("Puetas Bloqueada");
+ return;
+ }
+ if (!other.CompareTag("Player"))
+ return;
 
-        if (!other.CompareTag("Player"))
-            return;
-
-        roomController.UseDoor(this, other.transform);
-    }
-}
+ roomController.UseDoor(this, other.transform);
+ }
+ }
